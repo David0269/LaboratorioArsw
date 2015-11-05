1ce8e55f44cee8ac592ad494761a79d208430ba9
David <davidgarcia0269@gmail.com>
2015-11-05 13:09:54 -0500

    full

--- src/main/resources/static/js/app/controllers/blogctrl.js
+++ src/main/resources/static/js/app/controllers/blogctrl.js
@@ -1,7 +1,49 @@
 var app = angular.module("app",[]);
 
-app.controller("blogCtrl", function($scope) {
+app.controller("blogCtrl", function($scope,$log,$http) {
     $scope.entry = {title : "Title",
                     content : "Content"};
+    $scope.entries = [];
     $log.debug('se creo el $scope');
+
+    $scope.loadData = function() {
+        var configList = {
+                method: "GET",
+                url: "blogs"
+                    };
+
+        var response=$http(configList);
+
+        response.success(function(data, status, headers, config) {
+            $scope.entries = data;
+            });
+
+        response.error(function(data, status, headers, config) {
+            alert("Ha fallado la petición. Estado HTTP:"+status);
+            });
+    };
+    $scope.loadData();
 });
+
+   $scope.loadData = function() {
+        $http({
+            method: "GET",
+            url: "blogs"
+        }).success(function(data) {
+            $scope.entries = data;
+        }).error(function(data,status,headers,config) {
+            alert("Ha fallado la petición. Estado HTTP:"+status);
+        });
+    };
+
+$scope.processForm = function() {
+        $log.debug($scope.entry);
+        $http({
+            method  : 'POST',
+            url     : 'blog',
+            data    : $scope.entry
+        }).success(function(data) {
+            console.log(data);
+            $scope.loadData();
+        });
+    };
